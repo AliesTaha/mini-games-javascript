a70e3519d7f5b27f1d57fabaea2b49b3bc53c577
Ali Taha <ataha.blackberry.com>
2023-06-07 16:18:15 -0400

    -

--- Soccer_Javascript/soccer.js
+++ Soccer_Javascript/soccer.js
@@ -72,24 +72,60 @@ team2 < team1 && console.log('Team 1 is more likely to win');
 
 // 1. Loop over the game.scored array and print each player name to the console, along with 
 // the goal number (Example: "Goal 1: Lewandowski")
+for ([i, he] of game.scored.entries()){
+  console.log(`Goal ${i+1}: ${he}`);
+}
+
 
 // 2. Use a loop to calculate the average odd and log it to the console (We already studied 
 // how to calculate averages, you can go check if you don't remember)
+let avg=0;
+for (const odds of Object.values(game.odds)){
+  avg+=odds
+}
+avg/=Object.values(game.odds).length;
+console.log(avg);
+
 
 // 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
 //       Odd of victory Bayern Munich: 1.33
 //       Odd of draw: 3.25
 //       Odd of victory Borrussia Dortmund: 6.5
 // Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+for(const [team,odd] of Object.entries(game.odds)){
+  const teamStr = team ==='x' ? 'draw' : `victory ${game[team]}`
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
 
-// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
-//       {
-//         Gnarby: 1,
-//         Hummels: 1,
-//         Lewandowski: 2
-//       }
+// Coding Challenge #3
 
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
 
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+const arr= [... new Set(gameEvents.values())];
 
+console.log(arr);
 
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. 
+// So remove this event from the game events log.
+gameEvents.delete(64);
 
+// 3. Print the following string to the console: "An event happened, on average, every 9 
+// minutes" (keep in mind that a game has 90 minutes)
+console.log(`An event happened, on average, every ${90/gameEvents.size} minutes`);
+
+// 4. Loop over the events and log them to the console, marking whether it's in the first half or 
+// second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽️ GOAL
